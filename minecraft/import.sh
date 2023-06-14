df98745f8a3952f2a3eab4153625a005d329daca
root <root@TECNICO-BRUNO>
2023-06-14 15:17:45 -0300

    Minecraft: fix version docker-compose to 3

--- minecraft/import.sh
+++ minecraft/import.sh
@@ -2,8 +2,6 @@
 cd /root/save-game-server/
 git reset --hard HEAD
 git pull origin main
-# Variaveis de Container
-
 
 # Variaveis de Container
 SAVE_NAME='monga-vanila'
@@ -17,20 +15,16 @@ PATH_ACTUAL_SAVE='/root/data/minecraft/'
 FULLPATH_REPO="$PATH_REPO/$SAVE_NAME-$VERSION_BACKUP.tar.gz"
 FULLPATH_ACTUAL_SAVE="/root/data/minecraft/$SAVE_NAME/"
 
-# Pausando Container
-CONTAINER_ID=$(docker container ls --all | grep -w $IMAGE_NAME | awk '{print $1}')
-docker stop $CONTAINER_ID
-
 # Copiando Save $SAVE_NAME para o container
 #cd $(echo $PATH_ACTUAL_SAVE)
 #cd $(echo $REPO_DIR/minecraft/data/)
 BACK_SAVE="$REPO_DIR/minecraft/data/"
 
-cd $(echo $PATH_ACTUAL_SAVE)
-sudo rm -rf monga-vanila
-
+#cd $(echo $PATH_ACTUAL_SAVE)
+#sudo rm -rf monga-vanila
 cd $(echo $BACK_SAVE)
 cp -RT $PATH_REPO $FULLPATH_ACTUAL_SAVE
 
-# Iniciando Container
-docker start $CONTAINER_ID
+# Restartando Container
+CONTAINER_ID=$(docker container ls --all | grep -w $IMAGE_NAME | awk '{print $1}')
+docker restart $CONTAINER_ID
