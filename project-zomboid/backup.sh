37a67fcebd38e48235f29ff244905f3ec9660713
root <root@TECNICO-BRUNO>
2023-06-14 23:35:55 -0300

    Zomboid: fix backup and import

--- project-zomboid/backup.sh
+++ project-zomboid/backup.sh
@@ -3,24 +3,34 @@
 #docker container ls --all | grep -w $IMAGE_NAME | awk '{print $1}'
 #CONTAINER_ID='cbc1627fac72'
 cd ~/save-game-server/
-git pull
+git reset --hard HEAD
+git pull origin main
 
+SAVE_NAME='Monga'
+VERSION_BACKUP='v1.0'
+IMAGE_NAME='monga-project-zomboid'
+USER='/home/monga'
 
 # Variaveis de Container
-IMAGE_NAME='monga-project-zomboid'
 CONTAINER_SAVE='/home/steam/Zomboid/Saves/Multiplayer/Monga'
-REPO_SAVE='./saves-monga-server/'
-REPO_SAVE='./project-zomboid-v1.0/'
+DATA_SAVE="$USER/data/project-zomboid"
+PATH_REPO="$USER/save-game-server/project-zomboid/data"
 
-# Buscando Continer ID
+FULLPATH_REPO="$PATH_REPO/$SAVE_NAME/$SAVE_NAME"
+
+LOCAL_BACKUP=$DATA_SAVE/$SAVE_NAME.tar.gz
 CONTAINER_ID=$(docker container ls --all | grep -w $IMAGE_NAME | awk '{print $1}')
 
 # Copiando Save
-cd ~/save-game-server/
-docker cp $CONTAINER_ID:$CONTAINER_SAVE $REPO_SAVE
+cd $(echo $DATA_SAVE/$SAVE_NAME)
+sudo docker cp $CONTAINER_ID:$CONTAINER_SAVE $DATA_SAVE
 
 # Compactando Save e excluindo pasta
-tar -cvzf ./project-zomboid/project-zomboid-v1.0.tar.gz ./project-zomboid-v1.0/
+sudo tar -cvzf ./$SAVE_NAME.tar.gz ./
+sudo cp -RT $LOCAL_BACKUP $PATH_REPO
+
+/home/monga/save-game-server/project-zomboid/data/Monga/Monga
+
 sudo rm -rf ./project-zomboid-v1.0/
 
 # Commit Save
