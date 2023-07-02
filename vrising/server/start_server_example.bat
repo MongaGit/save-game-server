46ace8322f339a472a9cbc86a28c74fe409aa2b0
Bruno Gomes <brunoshy@gmail.com>
2023-07-02 00:16:43 -0300

    fix: git clone data saves

--- vrising/server/start_server_example.bat
+++ vrising/server/start_server_example.bat
@@ -4,4 +4,4 @@ set SteamAppId=1604030
 echo "Starting V Rising Dedicated Server - PRESS CTRL-C to exit"
 
 @echo on
-VRisingServer.exe -persistentDataPath .\save-data -serverName "My V Rising Server" -saveName "world1" -logFile ".\logs\VRisingServer.log"
+VRisingServer.exe -persistentDataPath .\save-data -serverName "Monga Server" -saveName "world1" -logFile ".\logs\VRisingServer.log"
